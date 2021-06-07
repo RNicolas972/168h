3198d47bd2744d22afd1a31aee3af854fece289f
hurung_a <hurung_a@etna-alternance.net>
2021-06-07 23:46:39 +0200

    WIP

--- src/components/LoadingComponent/LoadingComponent.js
+++ src/components/LoadingComponent/LoadingComponent.js
@@ -1,13 +1,36 @@
 import React, { useEffect } from 'react';
 import { ModelsLoader } from '../models/modelsLoader';
+import {
+    Color,
+    Scene,
+    WebGLRenderer,
+    PerspectiveCamera
+} from 'three'; 
 
 const LoadingComponent = () => {
+
+    const animate = ({ scene, camera }) => {
+        renderer.render(scene, camera);
+        requestAnimationFrame(animate);
+    }
+
+    const scene = new Scene();
+    const renderer = new WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    
+	scene.background = new Color(0xdddddd);
+    
+    document.body.appendChild(renderer.domElement);
     
     useEffect(() => {
         const test = async () => {
-            return new Promise(( res, rej ) => {
-                ModelsLoader.load("../../../public/assets/3d_models/dinosaur02.max");
-            });
+            await ModelsLoader.load( "dino.max", scene )
+                .then(() => {
+                    console.warn("LOADED: dino.max");
+                })
+                .catch(( err ) => {
+                    console.error(err);
+                });
         }
         ( async () => {
             await test();
@@ -16,7 +39,6 @@ const LoadingComponent = () => {
 
     return(
         <>
-            <span> Test </span>
         </>
     );
 }

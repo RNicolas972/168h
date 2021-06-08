fc4ec6d428b33582e4f5fc87612d415ebfbc4635
ramass_n <ramass_n@etna-alternance.net>
2021-06-08 09:35:20 +0200

    basic test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import * as THREE from 'three/build/three.module'
+import { GLTFLoader } from './js/GLTFLoader'
+
+
+
+// const geometry = new THREE.BoxGeometry();
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// const cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const loader = new GLTFLoader();
+
+  const scene = new THREE.Scene();
+  //const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 5000);
+
+  let hlight = new THREE.AmbientLight(0x404040, 100);
+  scene.add(hlight);
+
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  loader.load('models/scene.gltf',
+    // called when resource is loaded
+    function (object) {
+      scene.add(object)
+    },
+    // called when loading is in progresses
+    function (xhr) {
+      console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    },
+    // called when loading has errors
+    function (error) {
+      console.log('An error happened');
+      console.log(error)
+    }
   );
-}
+
+  return null;
+};
 
 export default App;

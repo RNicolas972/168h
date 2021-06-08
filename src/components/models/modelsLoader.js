a38bef29e85ea91babef18f12902d52e1c221e25
hurung_a <hurung_a@etna-alternance.net>
2021-06-08 11:34:24 +0200

    UPDATE: ModelsLoader loader

--- src/components/models/modelsLoader.js
+++ src/components/models/modelsLoader.js
@@ -2,16 +2,15 @@ import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
 
 export class ModelsLoader {
     static async load( filePath, scene, defaultPath='/assets/3d_models/') {
-        return new Promise(( res, rej ) => {
+        return new Promise( async ( res, rej ) => {
             const loader = new MTLLoader();
-
             loader.setPath(defaultPath);
-            loader.load(filePath, function (gltf) {
-                scene.add( gltf.scene );
-                res();
-            }, undefined, function (error) {
-                console.error(error);
-                rej();
+            loader.loadAsync(filePath)
+            .then(( file ) => {
+                res( file )
+            })
+            .catch(( err ) => {
+                console.error( err );
             });
         });
     }

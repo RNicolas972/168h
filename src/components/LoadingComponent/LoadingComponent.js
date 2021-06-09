7742db27f436ebfccae34af963b1015b2784baec
hurung_a <hurung_a@etna-alternance.net>
2021-06-09 15:20:30 +0200

    ADD: load component

--- src/components/LoadingComponent/LoadingComponent.js
+++ src/components/LoadingComponent/LoadingComponent.js
@@ -1,50 +1,64 @@
+import './LoadingComponent.css';
 import React, { useEffect } from 'react';
 import { ModelsLoader } from '../models/modelsLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import {
-    Mesh,
     Scene,
     Color,
-    PointLight,
+    AmbientLight,
     AxesHelper,
-    BoxGeometry,
     WebGLRenderer,
-    MeshBasicMaterial,
     PerspectiveCamera
 } from 'three';
 
 const LoadingComponent = () => {
     
-    const initScene = () => {
+    const initScene = async () => {
+
+        const canvas = document.getElementById('totoro-renderer');
+        const containerWidth = document.querySelector('div.main-container').offsetWidth;
+        const renderer = new WebGLRenderer({ canvas });
+
+        renderer.setSize( containerWidth, containerWidth / 2 );
+        document.querySelector('div.main-container').appendChild( renderer.domElement );
+
+        const sceneObj = [];
         const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-        const light = new PointLight(0xff0000, 10, 500);
+        const light = new AmbientLight(0xFFFFF, 1);
+        sceneObj.push( light );
         // SCENE SETUP
         const scene = new Scene();
-        scene.background = new Color("rgb(0,0,0)");
+        scene.background = new Color("rgb(38, 118, 255)");
+        
         // CAMERA SETUP
         camera.position.x = 3;
         camera.position.z = 5;
         camera.lookAt(0,0,0);
+
+        sceneObj.push( camera );
         // AXESHELPER SETUP
         const axesHelper = new AxesHelper( 500 );
-        // CUBE SETUP
-        const geometry = new BoxGeometry();
-        const material = new MeshBasicMaterial({ color: 0x00ff00 });
-        const cube = new Mesh( geometry, material );
-        cube.position.set( 0, 0, 0 );
-        
+        sceneObj.push( axesHelper );
+
         const animate = () => {
             requestAnimationFrame( animate );
-            renderer.render( scene, camera )
+            renderer.render( scene, camera );
         }
         
-        const renderer = new WebGLRenderer();
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        document.body.appendChild( renderer.domElement );
         // ORBITCONTROLS SETUP
         const orbit = new OrbitControls(camera, renderer.domElement);
+        sceneObj.push( orbit );
         
-        [ camera, light, cube, axesHelper, orbit ].forEach(( elt ) => {
+        const totoro = await ModelsLoader.load("totoro.glb");
+        
+        totoro.position.set( 0, 0.85, 0 );
+        totoro.scale.x = 0.5;
+        totoro.scale.y = 0.5;
+        totoro.scale.z = 0.5;
+        
+        sceneObj.push( totoro );
+
+        sceneObj.forEach(( elt ) => {
             scene.add( elt );
         });
 
@@ -52,26 +66,15 @@ const LoadingComponent = () => {
     }
 
     useEffect(() => {
-        // const test = async () => {
-        //     return new Promise(( res, rej ) => {
-        //         ModelsLoader.load("../../../public/assets/3d_models/dinosaur02.max")
-        //         .then(() => {
-        //             res();
-        //         })
-        //         .catch(() => {
-        //             rej();
-        //         });
-        //     });
-        // }
         ( async () => {
-            // await test();
-            initScene();
+            await initScene();
         })()
     });
 
     return(
-        <>
-        </>
+        <div className="main-container">
+            <canvas id="totoro-renderer"></canvas>
+        </div>
     );
 }
 

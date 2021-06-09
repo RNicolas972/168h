fb49d93963b433be449f5b4075a29b6ffb5b099a
hurung_a <hurung_a@etna-alternance.net>
2021-06-09 10:11:48 +0200

    ADD: GLTF Loader

--- src/components/models/modelsLoader.js
+++ src/components/models/modelsLoader.js
@@ -1,9 +1,26 @@
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 export class ModelsLoader {
-    static async load( filePath, scene, defaultPath='/assets/3d_models/') {
+    static async load( filePath, defaultPath='/assets/3d_models/') {
         return new Promise( async ( res, rej ) => {
-            const loader = new MTLLoader();
+            let loader = null;
+            
+            const regex2 = new RegExp('/*.(?:max|gltf)');
+            const isMatch = filePath.match( regex2 )[0];
+            
+            switch( isMatch ) {
+                case '.max':
+                    loader = new MTLLoader();
+                    break;
+
+                case '.gltf':
+                    loader = new GLTFLoader();
+                    break;
+                default:
+                    console.error(`ERROR when trying to load ${filePath} with format: ${isMatch}`);
+            }
+
             loader.setPath(defaultPath);
             loader.loadAsync(filePath)
             .then(( file ) => {

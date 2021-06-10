0809ebf0356c80024587f91dc7931c18e7af2cbd
ramass_n <ramass_n@etna-alternance.net>
2021-06-10 18:25:09 +0200

    WIP - Model Animation

---
+++ src/components/AnimationComponent/AnimationComponent.js
@@ -0,0 +1,80 @@
+import React, { useEffect } from 'react';
+import { ModelsLoader } from '../models/modelsLoader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import {
+    Scene,
+    Color,
+    AmbientLight,
+    AxesHelper,
+    WebGLRenderer,
+    PerspectiveCamera
+} from 'three';
+
+const AnimationComponent = () => {
+    
+    const initScene = async () => {
+
+        const canvas = document.getElementById('totoro-renderer');
+        const containerWidth = document.querySelector('div.main-container').offsetWidth;
+        const renderer = new WebGLRenderer({ canvas });
+
+        renderer.setSize( containerWidth, containerWidth / 2 );
+        document.querySelector('div.main-container').appendChild( renderer.domElement );
+
+        const sceneObj = [];
+        const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+        const light = new AmbientLight(0xFFFFF, 1);
+        sceneObj.push( light );
+        // SCENE SETUP
+        const scene = new Scene();
+        scene.background = new Color("rgb(38, 118, 255)");
+        
+        // CAMERA SETUP
+        camera.position.x = 3;
+        camera.position.z = 5;
+        camera.lookAt(0,0,0);
+
+        sceneObj.push( camera );
+        // AXESHELPER SETUP
+        const axesHelper = new AxesHelper( 500 );
+        sceneObj.push( axesHelper );
+
+        const animate = () => {
+            requestAnimationFrame( animate );
+            renderer.render( scene, camera );
+        }
+        
+        // ORBITCONTROLS SETUP
+        const orbit = new OrbitControls(camera, renderer.domElement);
+        sceneObj.push( orbit );
+        
+        const model = await ModelsLoader.load("wraith.glb");
+        model.position.set( 0, 0.85, 0 );
+        model.scale.set(1,1,1);
+        
+        sceneObj.push( model );
+
+        sceneObj.forEach(( elt ) => {
+            scene.add( elt );
+        });
+        
+        animate();
+    }
+
+
+    useEffect(() => {
+        ( async () => {
+            await initScene();
+        })()
+    });
+
+    return(
+        <div className="main-container">
+            <canvas id="totoro-renderer"></canvas>
+        </div>
+    );
+}
+
+export {
+    AnimationComponent
+}
--- src/components/router.js
+++ src/components/router.js
@@ -5,6 +5,7 @@ import {
 } from "react-router-dom";
 import { LoadingComponent } from './LoadingComponent/LoadingComponent';
 import { MovingComponent } from "./MovingComponent/MovingComponent";
+import { AnimationComponent } from "./AnimationComponent/AnimationComponent";
 
 export const AppRouter = () => {
     return (
@@ -17,6 +18,9 @@ export const AppRouter = () => {
             <Switch>
                 <Route path="/moving">
                     <MovingComponent />
+                </Route>
+                <Route path="/animation">
+                    <AnimationComponent />
                 </Route>
             </Switch>
         </div>
